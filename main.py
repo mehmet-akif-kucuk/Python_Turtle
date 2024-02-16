907c676f46f165be7df001e8def6f54da1f415fa
Mehmet Akif Küçük <m.akif.kucukk@gmail.com>
2024-02-16 04:31:40 +0300

    Proje Bitti :)

--- main.py
+++ main.py
@@ -4,31 +4,57 @@ import random
 
 cizim_ekrani = turtle.Screen()
 cizim_ekrani.bgcolor("white")
-cizim_ekrani.title("Python Turtle")
+cizim_ekrani.title("Python Turtle Game")
 cizim_ekrani.screensize(500, 500)
 
+
+skor_tutucu = turtle.Turtle()
+skor_tutucu.hideturtle()
+skor_tutucu.penup()
+skor_tutucu.color("green3")
+skor_tutucu.setpos(0, 350)
+skor = 0
+skor_tutucu.write("Puan: {}".format(skor), align="left", move=False, font=("Arial", 20, "normal"))
+
+sure_bilgi = int(turtle.numinput("Merhaba Oyuncu", "Lütfen Süreyi Giriniz:"))
+sure_tutucu = turtle.Turtle()
+sure_tutucu.hideturtle()
+sure_tutucu.penup()
+sure_tutucu.color("green3")
+sure_tutucu.setpos(0, 320)
+sure_tutucu.write("Süre: {}".format(sure_bilgi), align="left", move=False, font=("Arial", 20, "normal"))
+
 player = turtle.Turtle()
-player.shape("turtle")
 player.color("DodgerBlue")
-player.shapesize(1)
+player.shape("turtle")
+player.shapesize(4)
 player.penup()
+player.speed(2)
 
-
-
-bilgi = int(turtle.numinput("Merhaba Oyuncu", "Lütfen Süreyi Giriniz:"))
-puan = 0
 def puan_guncelle(x, y):
-    global puan
-    puan += 1
-    player.clear()
-    player.write("Puan: {}".format(puan), align="right", font=("Arial", 16, "normal",))
-cizim_ekrani.onscreenclick(puan_guncelle)
-player.write("Puan: {}".format(puan), align="center", font=("Arial", 16, "normal"))
+    global skor
+    skor_tutucu.clear()
+    skor += 1
+    skor_tutucu.write("Puan: {}".format(skor), align="left", font=("Arial", 20, "normal"))
 
-for i in range(bilgi):
+def turtleye_tikla(x, y):
+    if player.distance(x, y) < 20:
+        puan_guncelle(x, y)
+
+baslangic_zamani = time.time()
+
+while time.time() - baslangic_zamani < sure_bilgi:
     x = random.randint(-200, 200)
     y = random.randint(-200, 200)
-    player.teleport(x, y)
-    time.sleep(1)  # Kaldırıldı
+    turtle.onscreenclick(turtleye_tikla)
+    kalan_sure = int(sure_bilgi - (time.time() - baslangic_zamani))
+    sure_tutucu.clear()
+    sure_tutucu.write("Süre: {}".format(kalan_sure), align="left", move=False, font=("Arial", 20, "normal"))
+    player.goto(x, y)
+    if kalan_sure <= 0:
+        oyun_bitti = True
+        sure_tutucu.clear()
+        skor_tutucu.clear()
+        sure_tutucu.write("Game Over", align="left", move=False, font=("Arial", 20, "normal"))
 
 turtle.mainloop()
